7b7fb0d2774129640d4b2552eaee127ee4d1db26
Lee Mracek <lee.mracek@gmail.com>
2016-11-13 01:53:29 +0000

    Reformat tests to proper comment format

--- controve/tests/test_main.cpp
+++ controve/tests/test_main.cpp
@@ -2,19 +2,18 @@
 
 #include "gtest/gtest.h"
 
-/**
- * @file test_main.cpp
- *
- * @brief A wrapper around the googletest library to allow for commandline
- * options to be passed in for our libraries in a uniform manner.
- *
- * By adding commandline options to this file we can generalize the setup of
- * our tests by using this file instead of including googletest itself.
- *
- * @author Lee Mracek
- *
- * @bug No known bugs
- */
+///
+/// @file test_main.cpp
+///
+/// @brief A wrapper around the googletest library to allow for commandline
+/// options to be passed in for our libraries in a uniform manner.
+///
+/// By adding commandline options to this file we can generalize the setup of
+/// our tests by using this file instead of including googletest itself.
+///
+/// @author Lee Mracek
+///
+/// @bug No known bugs
 
 /// @brief A wrapper main for our test cases.
 GTEST_API_ int main(int argc, char **argv) {
